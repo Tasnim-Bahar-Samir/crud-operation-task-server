9d09cf2ec99f17c147a6a0318ee3229ab16f168f
Tasnim-Bahar-Samir <tanimsamir3@gmail.com>
2022-12-25 19:24:53 +0600

    created the api's

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors')
 const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
 const port = 5000 || process.env.PORT;
@@ -14,13 +14,51 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.dbUser}:${process.env.dbPassword}@cluster0.skbfv9j.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
+const dataCollection = client.db('tableData').collection('data');
 
-  console.log('db connected')
-  client.close();
-});
+async function run(){
+    try{
+        app.get('/data',async(req,res)=>{
+            const result = await dataCollection.find({}).toArray();
+            res.send({
+                success: true,
+                data: result
+            })
+        })
 
+        app.post('/data', async(req,res)=>{
+            const insertedData = req.body;
+            const result = await dataCollection.insertOne(insertedData);
+            if(result.insertedId){
+                res.send({
+                    success:true,
+                    message: "Data inserted successfully"
+                })
+            }else{
+                res.send({
+                    success:false,
+                    message: "Failed to insert"
+                })
+            }
+        })
+
+        app.patch('/data', async(req,res)=>{
+            const {id} = req.params;
+            const query = {_id : ObjectId(id)}
+            const result = await dataCollection.updateOne(query,{$set: req.body})
+        if(result.modifiedCount){
+            res.send({
+                success: true,
+                message: "Changes updated successfully."
+            })
+        }
+        })
+    }
+    catch(err){
+        console.log(err.message)
+    }
+}
+run()
  
 app.get('/',(req,res)=> {
     res.send('Server is running')
